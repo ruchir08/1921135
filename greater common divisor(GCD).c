8caf9f83f62068af95a8f9353695d3853fdb1214
ruchir08 <53931514+ruchir08@users.noreply.github.com>
2019-11-03 09:40:42 -0800

    Create greater common divisor(GCD).c

---
+++ greater common divisor(GCD).c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int m,n,r=1;
+printf("\n Enter value for m and n:");
+scanf("%d %d" ,&m,&n);
+while(r!=0)
+{
+r=n%m;
+n=m;
+m=r;
+}
+printf("\n GCD=%d\n",n);
+return 0;
+}
