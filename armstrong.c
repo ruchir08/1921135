57a511d3143a0f39803a61fd00437d53bbcf1d9c
ruchir08 <53931514+ruchir08@users.noreply.github.com>
2019-11-03 09:12:35 -0800

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int n,r,c,sum=0,temp;
+printf(" Enter n value:");
+scanf("%d",&n);
+temp=n;
+while(n>0)              
+{
+r=n%10;                                             
+c=r*r*r;
+sum=sum+c;
+n=n/10;
+}
+n=temp;
+if(n==sum)
+printf("armstrong no.");
+else
+printf("Not an armstrong no:");
+return 0;
+}
+
+
+
+                                                                     
