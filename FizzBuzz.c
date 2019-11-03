868dee1379297b1125505c4cbb73b62b015baad2
ruchir08 <53931514+ruchir08@users.noreply.github.com>
2019-11-03 09:05:04 -0800

    Create FizzBuzz.c

---
+++ FizzBuzz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n;
+for(n=1;n<=30;n++)
+{
+if(n%3==0 && n%5==0)
+printf("FizzBuzz\n");
+else if(n%3==0)
+printf("Fizz\n");
+else if(n%5==0)
+printf("Buzz\n");
+else
+printf("\n %d \n",n);
+}
+return 0;
+}
+                                                    
