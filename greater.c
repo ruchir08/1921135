36b448694455c7fad7c015a413a86c8a2b97e72c
ruchir08 <53931514+ruchir08@users.noreply.github.com>
2019-11-03 09:35:54 -0800

    Create greater.c

---
+++ greater.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ int a,b;
+printf("Enter any 2 number:");
+scanf("%d %d",&a,&b);
+if(a>b)
+{
+printf("a is grater");
+}
+else                                                                 
+{
+printf("b is greater");
+}
+return 0;
+}
+
+                                                    
